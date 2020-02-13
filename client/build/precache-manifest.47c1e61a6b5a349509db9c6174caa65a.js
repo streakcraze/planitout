fbe2cf9262c96321280e0d9665bb51069a620c0b
Arnold Odhis <odhisoarnold@gmail.com>
2020-02-13 14:51:39 +0300

    add client/build

---
+++ client/build/precache-manifest.47c1e61a6b5a349509db9c6174caa65a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90647f0aadec8751eca0a5771467a956",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7d2a0e3e27286192cf3",
+    "url": "/static/css/main.906093e8.chunk.css"
+  },
+  {
+    "revision": "9edf77f5ba50daa17ab2",
+    "url": "/static/js/2.2e643307.chunk.js"
+  },
+  {
+    "revision": "90331928f9c634c5a0ad01df6ab3c463",
+    "url": "/static/js/2.2e643307.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7d2a0e3e27286192cf3",
+    "url": "/static/js/main.f7b5ab80.chunk.js"
+  },
+  {
+    "revision": "6bacd3be5b223877b30e",
+    "url": "/static/js/runtime-main.7a04b51b.js"
+  }
+]);
